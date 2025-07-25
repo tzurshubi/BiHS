85f198bb17e2640be0b4dbd3d54d0835c41032a3
tzurshubi <tzur.shubi@gmail.com>
2025-07-25 16:54:46 +0300

    graphs

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -4,7 +4,7 @@ import csv
 from collections import defaultdict
 
 # Set the directory containing the txt files
-directory = "/home/tzur-shubi/Documents/Programming/BiHS/results/2025_07_24"
+directory = "/home/tzur-shubi/Documents/Programming/BiHS/results/2025_07_25"
 
 # Regex patterns
 result_pattern = re.compile(r"(A\*|XMM):\s*(\d+)\s*,\s*(\d+)\s*\(expansions\s*,\s*time\[ms\]\)")
