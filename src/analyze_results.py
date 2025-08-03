25d9f665d65c9f5c7d1db817579d0ac1ad29c7ce
tzurshubi <tzur.shubi@gmail.com>
2025-08-03 15:07:36 +0300

    log times

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -4,7 +4,7 @@ import csv
 from collections import defaultdict
 
 # Set the directory containing the txt files
-directory = "/home/tzur-shubi/Documents/Programming/BiHS/results/2025_07_27"
+directory = "/home/tzur-shubi/Documents/Programming/BiHS/results/2025_07_28/snake"
 
 # Regex patterns
 result_pattern = re.compile(r"(A\*|XMM):\s*(\d+)\s*,\s*(\d+)\s*\(expansions\s*,\s*time\[ms\]\)")
